321f5ba772d3f18a38219c4e8f3f9e8c57cf1f9c
Gies Bouwman <gies.bouwman@alliander.com>
2021-12-14 12:20:24 +0100

    improvements day 13

--- 13.js
+++ 13.js
@@ -21,7 +21,8 @@ function fold(x,y) {
   console.log(`Paper currently has ${paper.flat().reduce((cum, e) => cum+e, 0)} dots`);
 }
 
-var dimY = 894, dimX = 1310;
+var folds = [655,447,327,223,163,111,81,55,40,27,13,6];
+var dimY = 2*folds[1], dimX = 2*folds[0];
 
 var paper = Array(dimY);
 for(i=0;i<=dimY;i++) paper[i] = Array(dimX).fill(0);
@@ -29,11 +30,12 @@ lines.forEach(l => {
   var coord =  l.split(',');
   paper[parseInt(coord[1])][parseInt(coord[0])] = 1;
 });
-var folds = [655,447,327,223,163,111,81,55,40,27,13,6];
+// first 10 folds alternate, first along x (vertical) then y (horizontal)
 for (var f=0; f<5; f++) {
   fold(folds[2*f], -1 * (f>0 ? folds[2*f-1] : dimY+1));
   fold(-1 * folds[2*f], folds[2*f+1]);
 }
+// last 2 folds only along y
 fold(-1 * folds[8], folds[10]);
 fold(-1 * folds[8], folds[11]);
 
