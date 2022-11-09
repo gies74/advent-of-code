b589cc2abcba1c90d45b3a9067018e702426b812
Gies Bouwman <github@gies.dds.nl>
2022-11-09 23:01:47 +0100

    update

--- 2022/src/code-template.ts
+++ 2022/src/code-template.ts
@@ -2,9 +2,15 @@ namespace day00 {
     const fs = require("fs");
 
     const main = () => {
-        const input = fs.readFileSync(`${__dirname}\\..\\..\\aoc\\day00\\input.txt`).toString().split("\n");
-        console.log(input);
+        const input = fs.readFileSync(`${__dirname}\\..\\..\\aoc\\day00\\input.txt`).toString().split("\n").slice(0, -1);
+        const result = processInput(input);
+        console.log(`Antwoord: ${result}`);
     }
-    
+
+    const processInput = (input) => {
+        /** insert logic here */
+        return 0;
+    }
+
     main();
 }
