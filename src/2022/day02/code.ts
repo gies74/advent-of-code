bf4823bc61d1971bdbc658444a11c6b3e1e53b93
Gies Bouwman <github@gies.dds.nl>
2022-12-02 07:40:51 +0100

    day 2

---
+++ src/2022/day02/code.ts
@@ -0,0 +1,60 @@
+/**
+ * Advent of Code solution 2022/day02
+ * (c) 2022 Gies Bouwman
+ * gies.bouwman@alliander.com
+ * All rights reserved.
+ */
+
+import { Part, Utils } from "../../generic";
+
+namespace day02 {
+    
+    /** ADD 2022-day02 SPECIFIC OBJECTS, CLASSES AND FUNCTIONS HERE  */
+
+    Utils.main(
+        /**
+         * Main entry point of this day's code
+         * @param input this day's input
+         * @param part report answer for either part one or two
+         * @returns sought answer of given puzzle part
+         */
+        (input: string[], part: Part) => {
+
+            const elveTurn = { "A": "Rock", "B": "Paper", "C": "Scissors" }
+            const game = { 
+                "Rock": {"beats":"Scissors", "score":1}, 
+                "Paper": {"beats":"Rock", "score":2}, 
+                "Scissors": {"beats":"Paper", "score": 3}
+            };
+
+
+            if (part == Part.One) {
+                const myTurn = { "X": "Rock", "Y": "Paper", "Z": "Scissors" }
+                let score = 0;
+                for (var line of input) {
+                    const [elve, my] = line.split(' ');
+                    const eO = elveTurn[elve];
+                    const mO = myTurn[my];
+                    score += game[mO].score;
+                    score += (mO == eO) ? 3 : game[mO].beats == eO ? 6 : 0;
+                }
+                let answerPart1 = score;
+                return answerPart1;
+            } else {
+                let score = 0;
+                const myTurn = { "X": 0, "Y": 3, "Z": 6 };
+                for (var line of input) {
+                    const [elve, my] = line.split(' ');
+                    const eO = elveTurn[elve];
+                    const mS = myTurn[my];
+                    score += mS == 3 ? game[eO].score : mS==0 ? game[game[eO].beats].score : game[game[game[eO].beats].beats].score;
+                    score += myTurn[my];
+                }
+                let answerPart2 = score;
+                return answerPart2;
+            }
+
+        }, "2022", "day02", 
+        // set this switch to Part.Two once you've finished part one.
+        Part.Two);
+}
