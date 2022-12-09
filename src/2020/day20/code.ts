4fdca13380a92536a7d9cad2fcc9027ef2877c33
Gies Bouwman <github@gies.dds.nl>
2022-12-09 17:18:34 +0100

    part one

--- src/2020/day20/code.ts
+++ src/2020/day20/code.ts
@@ -18,26 +18,24 @@ namespace day20 {
         constructor(chunk: string[]) {
             this.id = parseInt(chunk[0].split(/[ :]/g)[1])
             this.data = chunk.slice(1);
+            this._updateEdges();
+        }
 
+        _updateEdges() {
             this.edges = [
                 this.data[0],
-                this.data[this.data.length - 1],
-                this.data.map(s => s.substring(0, 1)).join(''),
-                this.data.map(s => s.substring(s.length - 1)).join('')
+                this.data.map(s => s.substring(s.length - 1)).join(''),
+                this.data[this.data.length - 1].split('').reverse().join(''),
+                this.data.map(s => s.substring(0, 1)).reverse().join(''),
             ];
-            for (var e of this.edges) {
-                let num;
-                num = parseInt(e.replace(/#/g, '1').replace(/\./g, '0'), 2);
-                this.edgeNums.push(num);
-                num = parseInt(e.split('').reverse().join('').replace(/#/g, '1').replace(/\./g, '0'), 2);
-                this.edgeNums.push(num);
-            }
+            this.edgeNums = this.edges.map(e => parseInt(e.replace(/#/g, '1').replace(/\./g, '0'), 2));
+            this.edgeNums = this.edgeNums.concat(this.edges.map(e => parseInt(e.split('').reverse().join('').replace(/#/g, '1').replace(/\./g, '0'), 2)));
         }
 
         neighbours(tiles: Tile[]) {
             let cnt = 0;
             const edgeHavingNeighbour = [];
-            for (var i=0; i<this.edgeNums.length;i+=2) {
+            for (var i=0; i<4; i++) {
                 const num = this.edgeNums[i];
                 var neighbour = tiles.filter(t => t !== this).find(t => t.edgeNums.includes(num));
                 edgeHavingNeighbour.push(neighbour);
@@ -45,6 +43,45 @@ namespace day20 {
             return edgeHavingNeighbour;
         }
 
+        rightNeighbour(tiles) {
+            return this._neighbour(tiles, 1);
+        }
+        underNeighbour(tiles) {
+            return this._neighbour(tiles, 2);
+        }
+
+        _neighbour(tiles, edgeIndex) {
+            const myEdgeNum = this.edgeNums[edgeIndex];
+            const myRightNeighbour = this.neighbours(tiles)[edgeIndex];
+            let idx = myRightNeighbour.edgeNums.indexOf(myEdgeNum);
+            if (idx === -1)
+                throw Error("Fck");
+            if (idx < 4)
+                myRightNeighbour.flip();
+            idx = myRightNeighbour.edgeNums.indexOf(myEdgeNum);
+            if (idx < 4)
+                throw Error("fck more");
+            var rotations = (idx + 2 - edgeIndex) % 4;
+            for (var r=0; r<rotations; r++)
+                myRightNeighbour.rotate();
+            return myRightNeighbour;
+        }
+
+
+
+
+        flip() {
+            this.data = this.data.reverse();
+            this._updateEdges();
+        }
+
+        rotate() {
+            const data = [];
+            for (var i = this.data[0].length-1; i>=0; i--)
+                data.push(this.data.map(r => r[i]).join(''));
+            this.data = data;
+            this._updateEdges();
+        }
 
     }
 
@@ -67,22 +104,26 @@ namespace day20 {
             const tiles = chunks.map(ch => new Tile(ch));
 
             const cornerTiles = tiles.filter(tile => tile.neighbours(tiles).filter(n => n).length === 2);
+            for (var x=0; x<3; x++)
+                cornerTiles[0].rotate();
+            puzzle[0][0] = cornerTiles[0];
             if (cornerTiles.length != 4)
                 console.warn("FFFFFOOUOOOOUUUUUUTTTTT");
 
-            if (part == Part.One) {
+            for (var y=0; y < puzzle.length;y++) {
+                for (var x=1; x < puzzle[0].length; x++) {
+                    puzzle[y][x] = puzzle[y][x-1].rightNeighbour(tiles);
+                }
+                if (y < puzzle.length - 1) {
+                    puzzle[y+1][0] = puzzle[y][0].underNeighbour(tiles);
+                }
+            } 
 
+            if (part == Part.One) {
                 let answerPart1 = cornerTiles.reduce((agg, elt) => elt.id * agg, 1);
                 return answerPart1;
-
             } else {
-
-                
-
-                // part 2 specific code here
-
                 return 0;
-
             }
 
         }, "2020", "day20", 
