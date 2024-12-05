baa9e0704b9f48e13dee3599da417261c2a48715
Gies Bouwman <gies.bouwman@alliander.com>
2024-12-05 08:54:21 +0100

    sorter

---
+++ src/2024/day05/code.ts
@@ -0,0 +1,42 @@
+/**
+ * Advent of Code solution 2024/day05
+ * (c) 2022-2024 Gies Bouwman
+ * gies.bouwman@alliander.com
+ * All rights reserved.
+ */
+
+import { Part, Utils } from "../../generic";
+
+namespace day05 {
+
+    Utils.main(
+        /**
+         * Main entry point of this day's code
+         * @param input this day's input
+         * @param part report answer for either part one or two
+         * @param example example number (where 0 means: input containss actual puzzle input)
+         * @returns sought answer of given puzzle part
+         */
+        (input: string[], part: Part, example: number = 0) => {
+
+            const [rules, updates] = Utils.splitInput(input);
+            const sorter = (a,b) => rules.includes(a+'|'+b) ? -1 : rules.includes(b+'|'+a) ? 1 : 0;
+
+            let sum=0;
+            updates.forEach(update => {
+                const pages = update.split(',');
+                const sortedPages = pages.slice(0).sort(sorter);
+                const sortedUpdate = sortedPages.join(',');
+                if (part === Part.One && update === sortedUpdate || part === Part.Two && update !== sortedUpdate) {
+                    sum += parseInt(sortedPages[(sortedPages.length-1)/2]);
+                }
+            });
+
+            return sum;
+
+        }, "2024", "day05", 
+        // set this switch to Part.Two once you've finished part one.
+        Part.One, 
+        // set this to N > 0 in case you created a file called input_exampleN.txt in folder data/YEAR/DAY
+        0);
+}
