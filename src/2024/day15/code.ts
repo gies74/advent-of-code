a0b47148a23b06979d1f85488a22ca14934e9308
Gies Bouwman <github@gies.dds.nl>
2024-12-15 19:58:35 +0100

    d15.2 too lazy to merge with 15.1

--- src/2024/day15/code.ts
+++ src/2024/day15/code.ts
@@ -11,6 +11,24 @@ namespace day15 {
 
     const vector = { ">": [0, 1], "v": [1, 0], "<": [0, -1], "^": [-1, 0]};
 
+    const getMovingCoords = (ptrs:number[][], instr:string, grid:string[][]) => {
+        const pushDir = vector[instr];
+        const newBoxCoords = [];
+        for (var ptr of ptrs) {
+            const pusherSymb = grid[ptr[0]][ptr[1]]; 
+            let pushedSymb = grid[ptr[0] + pushDir[0]][ptr[1] + pushDir[1]];
+            if (pushedSymb === "#")
+                return null;
+            if ("[]".includes(pushedSymb))
+                newBoxCoords.push([ptr[0] + pushDir[0],ptr[1] + pushDir[1]]);
+            if ("^v".includes(instr) && pushedSymb == "]")
+                newBoxCoords.push([ptr[0] + pushDir[0], ptr[1] - 1]);
+            if ("^v".includes(instr) && pushedSymb == "[")
+                newBoxCoords.push([ptr[0] + pushDir[0], ptr[1] + 1]);
+        }
+        return newBoxCoords;
+    }
+
     Utils.main(
         /**
          * Main entry point of this day's code
@@ -23,32 +41,37 @@ namespace day15 {
 
             const [input0, input1] = Utils.splitInput(input);
 
-            const grid = input0.map(line => line.split(""));
+            const grid = input0.map(line => line.replace(/#/g, "##").replace(/\./g, "..").replace(/O/g, "[]").replace(/@/g, "@.").split(""));
             let pos = [];
             grid.forEach((line, li) => line.forEach((cell, ci) => { if (cell === "@") { pos[0]=li; pos[1]=ci; } }));
 
             const instrs = input1.join("").split("");
-            let instr:number[];
-            while (instr = vector[instrs.shift()]) {
+            let instr:string;
+            while (instr = instrs.shift()) {
 
-                let ptr = pos.slice(0);
-                let boxSeen = false;
-                let symb = null;
-                do {
-                    ptr[0] += instr[0]; ptr[1] += instr[1];
-                    symb = grid[ptr[0]][ptr[1]];
-                    boxSeen ||= symb === "O";
-                } while (symb === "O");
-                if (symb === ".") {
-                    if (boxSeen)
-                        grid[ptr[0]][ptr[1]] = "O";
-                    grid[pos[0]][pos[1]] = ".";
-                    pos = [pos[0] + instr[0], pos[1] + instr[1]];
-                    grid[pos[0]][pos[1]] = "@";
+                const allMovingCoords = [pos];
+                let newMovingCoords = getMovingCoords([pos], instr, grid);
+                while (newMovingCoords && newMovingCoords.length > 0) {
+                    newMovingCoords.forEach(c => {
+                        if (!allMovingCoords.some(apc => apc[0] === c[0] && apc[1] === c[1]))
+                            allMovingCoords.push(c);
+                    });
+                    newMovingCoords = getMovingCoords(newMovingCoords, instr, grid);
+                    if (newMovingCoords === null)
+                        break;
                 }
+                if (newMovingCoords === null)
+                    continue;
+                let ptr = null;
+                while (ptr = allMovingCoords.pop()) {
+                    grid[ptr[0] + vector[instr][0]][ptr[1] + vector[instr][1]] = grid[ptr[0]][ptr[1]];
+                    grid[ptr[0]][ptr[1]] = ".";
+                }
+                pos = [pos[0]+ vector[instr][0], pos[1]+ vector[instr][1]];
+
             }
 
-            const gps = grid.reduce((cum0, line, li) => cum0 + line.reduce((cum1, cell, ci) => cum1 + (cell === "O" ? li * 100 + ci : 0), 0), 0);
+            const gps = grid.reduce((cum0, line, li) => cum0 + line.reduce((cum1, cell, ci) => cum1 + (cell === "[" ? li * 100 + ci : 0), 0), 0);
 
             return gps;
 
